26ec22c1496332db4d71bdff857415c7e44d1d25
PedroErnestoDev <ernestopedro284@gmail.com>
2025-10-19 12:51:45 -0300

    Adicionei a seção about e technologies

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let menu = document.querySelector(".container-hamburguer")
-let menuItens = document.querySelector(".mobile-tools")
+let menu = document.querySelector(".container-hamburguer");
+let menuItens = document.querySelector(".mobile-tools");
 
 menu.addEventListener('click', () => {
-  if (menuItens.style.display == "none") {
-    menuItens.style.display = "flex"
+  let displayAtual = getComputedStyle(menuItens).display;
+
+  if (displayAtual === "none") {
+    menuItens.style.display = "flex";
   } else {
-    menuItens.style.display = "none"
+    menuItens.style.display = "none";
   }
 });
